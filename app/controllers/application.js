d46a0c03fa9e660b46565739c7cd87aacee213f6
mathijsjansen1987 <mathijs@code.rehab>
2016-02-24 21:15:13 +0100

    fix

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -18,25 +18,25 @@ export default Ember.Controller.extend({
 
     invalidateSession() {
       this.get('session').invalidate();
-    }
+    },
 
     toggleNav: function(){
-    Ember.$('#mobileNav').toggleClass('show');
-  },
+      Ember.$('#mobileNav').toggleClass('show');
+    },
 
-  checkConnection: function() {
-    var networkState = navigator.network.connection.type;
+    checkConnection: function() {
+      var networkState = navigator.network.connection.type;
 
-    var states = {};
-    states[Connection.UNKNOWN]  = 'Unknown connection';
-    states[Connection.ETHERNET] = 'Ethernet connection';
-    states[Connection.WIFI]     = 'WiFi connection';
-    states[Connection.CELL_2G]  = 'Cell 2G connection';
-    states[Connection.CELL_3G]  = 'Cell 3G connection';
-    states[Connection.CELL_4G]  = 'Cell 4G connection';
-    states[Connection.CELL]     = 'Cell generic connection';
-    states[Connection.NONE]     = 'No network connection';
-    /*
+      var states = {};
+      states[Connection.UNKNOWN]  = 'Unknown connection';
+      states[Connection.ETHERNET] = 'Ethernet connection';
+      states[Connection.WIFI]     = 'WiFi connection';
+      states[Connection.CELL_2G]  = 'Cell 2G connection';
+      states[Connection.CELL_3G]  = 'Cell 3G connection';
+      states[Connection.CELL_4G]  = 'Cell 4G connection';
+      states[Connection.CELL]     = 'Cell generic connection';
+      states[Connection.NONE]     = 'No network connection';
+      /*
 			if(states[networkState] == "WiFi connection"){
 				this.status = 'WIFI';
 				Ember.$('#status').css('color','green');
@@ -49,73 +49,73 @@ export default Ember.Controller.extend({
 			}*/
 
 
-    // alert('Connection type: ' + states[networkState]);
-  },
+      // alert('Connection type: ' + states[networkState]);
+    },
 
-  captureVideo: function(){
-    var self = this;
-    // capture callback
-    var captureSuccess = function(mediaFiles) {
-      var i, path, len;
-      for (i = 0, len = mediaFiles.length; i < len; i += 1) {
-        path = mediaFiles[i].fullPath;
-        self.send('showVideoPopup',path);
+    captureVideo: function(){
+      var self = this;
+      // capture callback
+      var captureSuccess = function(mediaFiles) {
+        var i, path, len;
+        for (i = 0, len = mediaFiles.length; i < len; i += 1) {
+          path = mediaFiles[i].fullPath;
+          self.send('showVideoPopup',path);
+        }
+      };
+
+      // capture error callback
+      var captureError = function(error) {
+        navigator.notification.alert('Error code: ' + error.code, null, 'Capture Error');
+      };
+
+      // start video capture
+      navigator.device.capture.captureVideo(captureSuccess, captureError, {limit:1});
+
+    },
+
+    showVideoPopup: function(path){
+
+      self = this;
+      Ember.$('#videoAddPopup').show();
+      Ember.$('#videoAddPopupBtn').click(function(){
+        self.send('createVideo',path);
+      });
+    },
+
+    createVideo: function(path){
+
+      Ember.$('#videoAddPopup').hide();
+      self = this;
+      var title = 'from'
+      var description = 'mobile';
+      var url = path;
+
+
+      function transitionToPost(video) {
+        self.transitionToRoute('videos');
       }
-    };
 
-    // capture error callback
-    var captureError = function(error) {
-      navigator.notification.alert('Error code: ' + error.code, null, 'Capture Error');
-    };
+      navigator.createThumbnail(path, function(err, imageData) {
+        if (err)
+          throw err;
 
-    // start video capture
-    navigator.device.capture.captureVideo(captureSuccess, captureError, {limit:1});
+        self.set('thumb',imageData); // Will log the base64 encoded string in console.
+      });
 
-  },
+      var video = this.store.createRecord('video', {
+        title: title,
+        description: description,
+        url: url,
+      });
 
-  showVideoPopup: function(path){
+      function failure(reason) {
+        // handle the error
+        console.log(reason);
+        alert('failure:'+reason + ":" + reason.message);
+      }
 
-    self = this;
-    Ember.$('#videoAddPopup').show();
-    Ember.$('#videoAddPopupBtn').click(function(){
-      self.send('createVideo',path);
-    });
-  },
-
-  createVideo: function(path){
-
-    Ember.$('#videoAddPopup').hide();
-    self = this;
-    var title = 'from'
-    var description = 'mobile';
-    var url = path;
-
-
-    function transitionToPost(video) {
-      self.transitionToRoute('videos');
+      video.save().then(transitionToPost).catch(failure);
     }
 
-    navigator.createThumbnail(path, function(err, imageData) {
-      if (err)
-        throw err;
-
-      self.set('thumb',imageData); // Will log the base64 encoded string in console.
-    });
-
-    var video = this.store.createRecord('video', {
-      title: title,
-      description: description,
-      url: url,
-    });
-
-    function failure(reason) {
-      // handle the error
-      console.log(reason);
-      alert('failure:'+reason + ":" + reason.message);
-    }
-
-    video.save().then(transitionToPost).catch(failure);
   }
-
-}
-                                       });
+});
